2038846ebf593a7894a1631904cca53ae49d9167
Jefferson da Silva Simplicio <jeffersonsimplicio.js@gmail.com>
2024-11-07 11:23:44 -0300

    refactor(dto): update city DTOs to align with model changes

--- src/TrybeHotel/Dto/CityDto.cs
+++ src/TrybeHotel/Dto/CityDto.cs
@@ -1,12 +1,14 @@
 namespace TrybeHotel.Dto;
 
-public class CityDto {
-    public int CityId { get; set; }
+public class CityBaseDto {
     public string Name { get; set; }
     public string State { get; set; }
 }
 
-public class CityDtoInsert {
-    public string Name { get; set; }
-    public string State { get; set; }
+public class CityDto : CityBaseDto {
+    public int CityId { get; set; }
 }
+
+public class CityCreateDto : CityBaseDto { }
+
+public class CityUpdateDto : CityBaseDto { }
